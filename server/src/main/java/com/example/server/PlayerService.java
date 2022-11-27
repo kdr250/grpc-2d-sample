b5ca2a0c295018b537004280951feb2c0731a1be
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-11-27 22:28:26 +0900

    serverのPlayer同期作成

---
+++ server/src/main/java/com/example/server/PlayerService.java
@@ -0,0 +1,49 @@
+package com.example.server;
+
+import com.example.shared.GrpcPlayer;
+import com.example.shared.PlayerGrpc;
+import com.example.shared.PlayerSyncRequest;
+import com.example.shared.PlayerSyncResponse;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@GRpcService
+public class PlayerService extends PlayerGrpc.PlayerImplBase {
+
+  private final RegisterPlayerToRedisComponent registerPlayerToRedisComponent;
+
+  public PlayerService(final RegisterPlayerToRedisComponent registerPlayerToRedisComponent) {
+    this.registerPlayerToRedisComponent = registerPlayerToRedisComponent;
+  }
+
+  @Override
+  public StreamObserver<PlayerSyncRequest> sync(StreamObserver<PlayerSyncResponse> responseObserver) {
+    return new StreamObserver<PlayerSyncRequest>() {
+      @Override
+      public void onNext(PlayerSyncRequest value) {
+        GrpcPlayer grpcPlayer = value.getPlayer();
+        registerPlayerToRedisComponent.register("world1", grpcPlayer);
+
+        List<Supplier<GrpcPlayer>> playerGetFunctionList = registerPlayerToRedisComponent.get("world1", grpcPlayer.getId());
+        for (Supplier<GrpcPlayer> s : playerGetFunctionList) {
+          PlayerSyncResponse playerSyncResponse = PlayerSyncResponse.newBuilder().setOtherPlayer(s.get()).build();
+          responseObserver.onNext(playerSyncResponse);
+        }
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        t.printStackTrace();
+      }
+
+      @Override
+      public void onCompleted() {
+        responseObserver.onCompleted();
+      }
+    };
+  }
+}
