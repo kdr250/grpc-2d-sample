f6fed39757003f3df82cfa40bb94c7a819a2d5ad
kdr250 <54400975+kdr250@users.noreply.github.com>
2022-12-08 10:07:33 +0900

    不要なimport削除

--- client/src/main/java/com/example/client/GamePanel.java
+++ client/src/main/java/com/example/client/GamePanel.java
@@ -8,8 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 public class GamePanel extends JPanel implements Runnable {
